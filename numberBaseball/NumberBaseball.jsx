7b1e3fa6d32b447dfc9ad0bf1a4a77ab080dd846
sh183694@gmail.com <sh183694@gamil.com>
2019-11-09 18:30:42 +0900

    CREATE numberBaseball

---
+++ numberBaseball/NumberBaseball.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NumberBaseball = () => {
+  return (
+    <>
+      <div>
+        <h1>숫자야구</h1>
+      </div>
+    </>
+  );
+};
+
+export default NumberBaseball;
