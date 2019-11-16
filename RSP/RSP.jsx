b20485c833fe5ecccc719abe835ab9273ab1bc45
sh183694@gmail.com <sh183694@gamil.com>
2019-11-16 18:24:23 +0900

    CREATE RSP game

--- RSP/RSP.jsx
+++ RSP/RSP.jsx
@@ -1,40 +1,102 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import img from "./src/images/rsp.jpg";
 
-const RspImg = styled.div`
-  background-image: url(${img});
-  width: 152px;
-  height: 148px;
-`;
+const scores = {
+  rock: 0,
+  scissors: 1,
+  paper: -1
+};
 
 class RSP extends Component {
   state = {
     result: "",
-    imgCoord: 0,
+    rsp: "rock",
     score: 0
   };
 
+  interval;
+
+  componentDidMount() {
+    this.interval = setInterval(this.changeHand, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  changeHand = () => {
+    const { rsp } = this.state;
+    if (rsp === "rock") {
+      this.setState({
+        rsp: "scissors"
+      });
+    } else if (rsp === "scissors") {
+      this.setState({
+        rsp: "paper"
+      });
+    } else if (rsp === "paper") {
+      this.setState({
+        rsp: "rock"
+      });
+    }
+  };
+
+  onClickBtn = choice => {
+    const { rsp } = this.state;
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const cpuScore = scores[rsp];
+
+    const diff = myScore - cpuScore;
+
+    if (diff === 0) {
+      this.setState({
+        result: "비겼습니다."
+      });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState(prevState => {
+        return {
+          result: "이겼습니다!",
+          score: prevState.score + 1
+        };
+      });
+    } else {
+      this.setState(prevState => {
+        console.log(cpuScore);
+        return {
+          result: "졌습니다...",
+          score: prevState.score - 1
+        };
+      });
+    }
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 1000);
+  };
+
   render() {
-    const { result, score, imgCoord } = this.state;
+    const { result, score, rsp } = this.state;
     return (
       <>
-        <RspImg id="computer" />
+        <h1>{rsp}</h1>
         <div>
-          <button id="rock" className="btn" onClick={() => onClickBtn("Rock")}>
+          <button
+            id="rock"
+            className="btn"
+            onClick={() => this.onClickBtn("rock")}
+          >
             Rock
           </button>
           <button
-            id="scissor"
+            id="scissors"
             className="btn"
-            onClick={() => onClickBtn("Scissor")}
+            onClick={() => this.onClickBtn("scissors")}
           >
             Scissor
           </button>
           <button
             id="paper"
             className="btn"
-            onClick={() => onClickBtn("Paper")}
+            onClick={() => this.onClickBtn("paper")}
           >
             Paper
           </button>
