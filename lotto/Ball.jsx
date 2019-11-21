98d723e0f53a1e7f54a8f465c0d5bf655ce3dcda
sh183694@gmail.com <sh183694@gamil.com>
2019-11-21 16:01:50 +0900

    ADD Ball

---
+++ lotto/Ball.jsx
@@ -0,0 +1,32 @@
+import React, { memo } from "react";
+import styled from "styled-components";
+
+const Styled = styled.div`
+  display: inline-block;
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: center;
+  margin-right: 20px;
+`;
+
+const Ball = memo(({ number }) => {
+  let background;
+  if (number <= 10) {
+    background = "red";
+  } else if (number <= 20) {
+    background = "orange";
+  } else if (number <= 30) {
+    background = "yellow";
+  } else if (number <= 40) {
+    background = "blue";
+  } else {
+    background = "green";
+  }
+  return <Styled style={{ background }}>{number}</Styled>;
+});
+
+export default Ball;
